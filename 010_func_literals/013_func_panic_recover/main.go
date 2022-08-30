42ab6cfc99b61d65e96efb51fbf60bffcb6504b2
OoThan <mgoothan.psnl@gmail.com>
2022-08-30 18:16:43 +0630

    go panic. Close  #1

--- 010_func_literals/013_func_panic_recover/main.go
+++ 010_func_literals/013_func_panic_recover/main.go
@@ -4,12 +4,15 @@ import "fmt"
 
 func main() {
 
-	defer func() {
-		errMsg := recover()
-		fmt.Println(errMsg)
-	}()
+	//defer func() {
+	//	errMsg := recover()
+	//	fmt.Println(errMsg)
+	//}()
 
-	var x map[string]int
-	x["key"] = 10
-	fmt.Println(x)
+	//var x map[string]int
+	//x["key"] = 10
+	//fmt.Println(x)
+
+	panic("Boo!")
+	fmt.Println("This line won't be reached.")
 }
