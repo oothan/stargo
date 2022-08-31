9becdebc4d15223a083a777fd4653a93433aa28a
OoThan <mgoothan.psnl@gmail.com>
2022-08-31 18:46:29 +0630

    Concurrency - atomic. Close #15

---
+++ 013_concurrency/06_atomic/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var waitG sync.WaitGroup
+var counter uint64
+
+func main() {
+	waitG.Add(2)
+	go numbers(1)
+	go numbers(2)
+	waitG.Wait()
+
+	fmt.Printf("\nCounter : %d", counter)
+}
+
+func numbers(callID int) {
+	rand.Seed(time.Now().UnixNano())
+	for i := 1; i <= 10; i++ {
+		time.Sleep(200 * time.Millisecond)
+
+		atomic.AddUint64(&counter, 1)
+		fmt.Printf("(%d) %d %d\n", callID, rand.Intn(20)+20, atomic.LoadUint64(&counter))
+	}
+	waitG.Done()
+}
