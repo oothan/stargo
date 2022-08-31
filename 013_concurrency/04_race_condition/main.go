8b32fcebdf456955b8b460c5f0ccc8147dc60164
OoThan <mgoothan.psnl@gmail.com>
2022-08-31 18:17:11 +0630

    Concurrency - Race Condition. Close #13

---
+++ 013_concurrency/04_race_condition/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var waitG sync.WaitGroup
+var counter = 0
+
+func main() {
+	waitG.Add(2)
+	go numbers(1)
+	go numbers(2)
+	waitG.Wait()
+
+	fmt.Printf("\ncounter: %d", counter)
+}
+
+func numbers(callID int) {
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i <= 10; i++ {
+		tmpCounter := counter
+		tmpCounter++
+
+		time.Sleep(200 * time.Millisecond)
+		counter = tmpCounter
+
+		fmt.Printf("(%d) %d %d \n", callID, rand.Intn(20)+20, counter)
+	}
+	waitG.Done()
+}
