60a0f2594da442cfe6fa9028a188ff1acf1e62c1
OoThan <mgoothan.psnl@gmail.com>
2022-09-06 11:41:48 +0630

    channel unbuffered. Close #16

--- 014_channel/04_channel_unbuffered/main.go
+++ 014_channel/04_channel_unbuffered/main.go
@@ -1,1 +1,31 @@
-package _4_channel_unbuffered
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var waitG sync.WaitGroup
+
+func main() {
+	c := make(chan int)
+
+	waitG.Add(1)
+	go send(c)
+	go receive(c)
+	waitG.Wait()
+}
+
+func send(c chan int) {
+	for i := 1; i < 6; i++ {
+		c <- i
+	}
+	waitG.Done()
+}
+
+func receive(c chan int) {
+	for {
+		fmt.Println(<-c)
+	}
+	//waitG.Done()
+}
